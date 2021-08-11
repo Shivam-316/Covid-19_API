74f4dfa4ce5d780f1860aac4edae6b76bd2760e3
Shivammm <coolshiv77@gmail.com>
2021-08-11 10:16:40 +0530

    Data Collection Script V1

--- scheduler/management/commands/_private.py
+++ scheduler/management/commands/_private.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import numpy as np
-import json
 import requests as req
 import io
 from datetime import datetime, date, timedelta
@@ -37,8 +35,13 @@ def india_vaccination():
     df_data = df_data.drop(columns=['Updated On'])
     df_data.insert(0, 'Date', date_list, True)
     df_data = df_data.loc[df_data["State"] == 'India']
-    df_data.shape
-    df_data = df_data.drop(columns=['Total Sessions Conducted','Total Sites ','Total Individuals Vaccinated','AEFI'])
+    df_data = df_data[[
+        'Date', 'State','First Dose Administered', 'Second Dose Administered',
+        'Male (Doses Administered)', 'Female (Doses Administered)','Transgender (Doses Administered)',
+        ' Covaxin (Doses Administered)', 'CoviShield (Doses Administered)', 'Sputnik V (Doses Administered)',
+        '18-44 Years (Doses Administered)','45-60 Years (Doses Administered)', '60+ Years (Doses Administered)',
+        'Total Individuals Vaccinated'
+    ]]
     df_data.reset_index(drop=True, inplace=True)
     return df_data
 
@@ -56,9 +59,9 @@ def state_code():
     data = pd.read_csv(io.StringIO(data_req.text))
     df_data = pd.DataFrame.from_dict(data)
     df_data = df_data.drop(index=[0])
-    key = df_data['State_code'].tolist()
-    key.remove('UN')
-    return key
+    df_data = df_data[['State','State_code']]
+    df_data.columns = ['State','key']
+    return df_data
 
 def state_covid_cases():
     data_req = req.get('https://api.covid19india.org/csv/latest/states.csv')
@@ -84,27 +87,30 @@ def state_vaccination():
     df = pd.DataFrame.from_dict(df)
     df = df.rename(columns={'Updated On':'Date'})
     df['Date'] = pd.to_datetime(df['Date'],format='%d/%m/%Y')
-    sorted_df = df.sort_values(by = ['Date','State'])
-    sorted_df = sorted_df.reset_index(drop = True)
-    indx = sorted_df.index[sorted_df['State'] == 'India'].tolist()
-    sorted_df = sorted_df.drop(index = indx,axis = 0)
-    sorted_df = sorted_df.reset_index(drop = True)
+    sorted = df.sort_values(by = ['Date','State'])
+    sorted = sorted.reset_index(drop = True)
+    indx = sorted.index[sorted['State'] == 'India'].tolist()
+    sorted = sorted.drop(index = indx,axis = 0)
+    sorted = sorted.reset_index(drop = True)
     before = (date.today()-timedelta(days = 30)).isoformat()
-    idx = sorted_df.index[sorted_df['Date']==before].tolist()
-    sorted_df = sorted_df.iloc[idx[0]:]
-    sorted_df = sorted_df.reset_index(drop = True)
-    sorted_df = sorted_df.drop(columns = ['Total Sites ','Total Individuals Vaccinated','Total Sessions Conducted','AEFI'])
-    return sorted_df
+    idx = sorted.index[sorted['Date']==before].tolist()
+    sorted = sorted.iloc[idx[0]:]
+    sorted = sorted.reset_index(drop = True)
+    sorted = sorted[[
+        'Date', 'State','First Dose Administered', 'Second Dose Administered',
+        'Male (Doses Administered)', 'Female (Doses Administered)','Transgender (Doses Administered)',
+        ' Covaxin (Doses Administered)', 'CoviShield (Doses Administered)', 'Sputnik V (Doses Administered)',
+        '18-44 Years (Doses Administered)','45-60 Years (Doses Administered)', '60+ Years (Doses Administered)',
+        'Total Individuals Vaccinated'
+    ]]
+    return sorted
 
 def state_data():
     df_cases = state_covid_cases()
     df_vaccination = state_vaccination()
     df_req = df_cases.merge(df_vaccination, how='outer')
-    keys = state_code()
-    num_of_rows = df_req.count()[0]
-    keys = keys*num_of_rows
-    keys = pd.Series(keys)
-    df_req.insert(loc=0, column='key', value = keys)
+    state_key = state_code()
+    df_req = state_key.merge(df_req, on='State')
     return df_req
 
 def post_to_database(row):
@@ -115,17 +121,19 @@ def post_to_database(row):
             headers = {'content-type': 'application/json'},
             auth = ('peter','peter316')
         )
+        print(row)
     except Exception as e:
-        print(e)
+        print(e, "POST Error")
 
 def init_data():
     try:
-        full_df = pd.concat([state_data(),india_data()],axis=0)
+        full_df = pd.concat([india_data(),state_data()],axis=0)
         full_df['Date'] = full_df['Date'].dt.strftime('%Y-%m-%d')
-        full_df.columns = ['key', 'date', 'state', 'confirmed', 'recovered', 'deceased', 'first_dose', 'second_dose', 'male_vcc', 'female_vcc','transgender_vcc','total_covaxin','total_covishield','total_sputnik','age18_45','age45_60','age60','total_vcc']
         full_df.fillna(value=0,inplace=True)
+        full_df.columns = ['key', 'date', 'state', 'confirmed', 'recovered', 'deceased', 'first_dose', 'second_dose', 'male_vcc', 'female_vcc','transgender_vcc','total_covaxin','total_covishield','total_sputnik','age18_45','age45_60','age60','total_vcc']
+
     except Exception as e:
-        print(e)
+        print(e, "Data Error")
     else:
         StatewiseData.objects.all().delete()
-        full_df.apply(post_to_database,axis=1)
+        full_df.iloc[:5].apply(post_to_database,axis=1)
